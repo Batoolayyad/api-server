0dd045feb52559513242951c5dc0c710a6b76002
Batoolayyad <batoolayyad1996@gmail.com>
2021-07-07 17:06:49 +0300

    sql deploy

--- src/models/food-schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS food;
-
-CREATE TABLE food(
-  id SERIAL PRIMARY KEY,
-  type varchar(255),
-  price INT
-);
--- src/models/schema.sql
+++ src/models/schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE clothes(
   type varchar(255),
   price INT
 );
+DROP TABLE IF EXISTS food;
+
+CREATE TABLE food(
+  id SERIAL PRIMARY KEY,
+  type varchar(255),
+  price INT
+);
